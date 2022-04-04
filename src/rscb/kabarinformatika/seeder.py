a6dc4c8916a69811dcf5beb5dfcc1aa28a345b5e
Fajar Ru <kzofajar@gmail.com>
2022-04-04 07:56:16 +0700

    Improve Kabar Informatika data loader and seeder

--- src/rscb/kabarinformatika/seeder.py
+++ src/rscb/kabarinformatika/seeder.py
@@ -1,13 +1,13 @@
 import random
 
 
-def select_random_events() -> set:
+def _select_random_events() -> set:
     return set(random.choices(
         ['REACT', 'COMMENT', 'SHARE'], weights=[3, 2, 1], k=random.choice([1, 2, 3])
     ))
 
 
-def select_random_article_ids(article_ids: list) -> set:
+def _select_random_article_ids(article_ids: list) -> set:
     return set(random.choices(
         article_ids, k=random.choice(range(round(len(article_ids) / 2), len(article_ids), 2))
     ))
@@ -22,8 +22,8 @@ def generate_interactions(article_ids: list, user_count: int) -> list:
     }
 
     for user_id in range(1, user_count + 1):
-        for article_id in select_random_article_ids(article_ids):
-            for event in select_random_events():
+        for article_id in _select_random_article_ids(article_ids):
+            for event in _select_random_events():
                 interactions.append({
                     'user_id': user_id,
                     'article_id': article_id,
